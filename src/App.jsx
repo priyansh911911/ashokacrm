b5ad83f15f15f7ea8ce8a1974e54d08cd7b30572
priyansh911911 <priyanshgkp9999@gmail.com>
2025-11-13 18:35:54 +0530

    df

--- src/App.jsx
+++ src/App.jsx
@@ -47,6 +47,7 @@ import Resturantreservation from "./components/Resturant/Resturantreservation.js
 import Billing from "./components/Resturant/Billing.jsx";
 import Payment from "./components/payment/Payment.jsx";
 import Invoice from "./components/Invoice.jsx";
+import POSInvoice from "./components/POSInvoice.jsx";
 import Users from "./components/Users/Users.jsx";
 import AddBooking from "./components/Banquet/pages/Students/AddBooking.jsx";
 import ListBooking from "./components/Banquet/pages/Students/ListBooking.jsx";
@@ -235,6 +236,7 @@ const App = () => {
                       />
                       <Route path="/payment" element={<Payment />} />
                       <Route path="/invoice" element={<Invoice />} />
+                      <Route path="/pos-invoice" element={<POSInvoice />} />
                       <Route path="/users" element={<Users />} />
                       <Route path="/add-booking" element={<AddBooking />} />
                       <Route
